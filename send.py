267e55036e9f25a39d1e371e731436137155a15d
VictoireMukovi <victoirepalukumukovi@gmail.com>
2024-03-24 22:45:34 +0100

    Initialiastion du projet

---
+++ send.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": "victoirepalukumukovi@gmail.com",
+    "MAIL_PASSWORD": "nrlw xtox sbim xtjz"
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+if __name__ == '__main__':
+    with app.app_context():
+        msg = Message(subject="Hello",
+                      sender=app.config.get("MAIL_USERNAME"),
+                      recipients=[""], # use your email for testing
+                      body="This is a test email I sent with Gmail and Python!")
+        mail.send(msg)
